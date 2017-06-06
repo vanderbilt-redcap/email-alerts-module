605b084e38d3392e1fe1d41f04984eabbbd353b1
bascome <eva.bascompte.moragas@vanderbilt.edu>
2017-06-06 10:24:20 -0500

    original order

--- saveForm.php
+++ saveForm.php
@@ -7,31 +7,13 @@ $pid = $_GET['pid'];
 
 #get data from the DB
 $form_name = empty(ExternalModules::getProjectSetting($prefix, $pid, 'form-name'))?array():ExternalModules::getProjectSetting($prefix, $pid, 'form-name');
-array_push($form_name,$_REQUEST['form-name']);
-ExternalModules::setProjectSetting($prefix,$pid, 'form-name', $form_name);
-
 $email_to = empty(ExternalModules::getProjectSetting($prefix, $pid, 'email-to'))?array():ExternalModules::getProjectSetting($prefix, $pid, 'email-to');
-array_push($email_to,$_REQUEST['email-to']);
-ExternalModules::setProjectSetting($prefix,$pid, 'email-to', $email_to);
-
 $email_cc =  empty(ExternalModules::getProjectSetting($prefix, $pid, 'email-cc'))?array():ExternalModules::getProjectSetting($prefix, $pid, 'email-cc');
-array_push($email_cc,$_REQUEST['email-cc']);
-ExternalModules::setProjectSetting($prefix,$pid, 'email-cc', $email_cc);
-
 $email_subject =  empty(ExternalModules::getProjectSetting($prefix, $pid, 'email-subject'))?array():ExternalModules::getProjectSetting($prefix, $pid, 'email-subject');
-array_push($email_subject,$_REQUEST['email-subject']);
-ExternalModules::setProjectSetting($prefix,$pid, 'email-subject', $email_subject);
-
 $email_text =  empty(ExternalModules::getProjectSetting($prefix, $pid, 'email-text'))?array():ExternalModules::getProjectSetting($prefix, $pid, 'email-text');
-array_push($email_text,$_REQUEST['email-text-editor']);
-ExternalModules::setProjectSetting($prefix,$pid, 'email-text', $email_text);
-
 $email_repetitive =  empty(ExternalModules::getProjectSetting($prefix, $pid, 'email-repetitive'))?array():ExternalModules::getProjectSetting($prefix, $pid, 'email-repetitive');
 $email_timestamp =  empty(ExternalModules::getProjectSetting($prefix, $pid, 'email-timestamp'))?array():ExternalModules::getProjectSetting($prefix, $pid, 'email-timestamp');
-
 $email_condition =  empty(ExternalModules::getProjectSetting($prefix, $pid, 'email-condition'))?array():ExternalModules::getProjectSetting($prefix, $pid, 'email-condition');
-array_push($email_condition,$_REQUEST['email-condition']);
-ExternalModules::setProjectSetting($prefix,$pid, 'email-condition', $email_condition);
 
 #checkboxes
 if(empty($_REQUEST['email-repetitive'])){
@@ -39,16 +21,33 @@ if(empty($_REQUEST['email-repetitive'])){
 }else{
     $repetitive = "1";
 }
-array_push($email_repetitive,$repetitive);
-ExternalModules::setProjectSetting($prefix,$pid, 'email-repetitive', $email_repetitive);
 
 if(empty($_REQUEST['email-timestamp'])){
     $timestamp = "0";
 }else{
     $timestamp = "1";
 }
+
+#Add new data with old
+array_push($form_name,$_REQUEST['form-name']);
+array_push($email_to,$_REQUEST['email-to']);
+array_push($email_cc,$_REQUEST['email-cc']);
+array_push($email_subject,$_REQUEST['email-subject']);
+array_push($email_text,$_REQUEST['email-text-editor']);
+array_push($email_repetitive,$repetitive);
 array_push($email_timestamp,$timestamp);
+array_push($email_condition,$_REQUEST['email-condition']);
+
+#Save data
+
+ExternalModules::setProjectSetting($prefix,$pid, 'form-name', $form_name);
+ExternalModules::setProjectSetting($prefix,$pid, 'email-to', $email_to);
+ExternalModules::setProjectSetting($prefix,$pid, 'email-cc', $email_cc);
+ExternalModules::setProjectSetting($prefix,$pid, 'email-subject', $email_subject);
+ExternalModules::setProjectSetting($prefix,$pid, 'email-text', $email_text);
+ExternalModules::setProjectSetting($prefix,$pid, 'email-repetitive', $email_repetitive);
 ExternalModules::setProjectSetting($prefix,$pid, 'email-timestamp', $email_timestamp);
+ExternalModules::setProjectSetting($prefix,$pid, 'email-condition', $email_condition);
 
 //Extra Data
 $email_sent =  empty(ExternalModules::getProjectSetting($prefix, $pid, 'email-sent'))?array():ExternalModules::getProjectSetting($prefix, $pid, 'email-sent');
