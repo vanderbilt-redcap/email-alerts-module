1d9c18ac3de1fd08853ce66c9a07d55ce766c7b1
bascome <eva.bascompte.moragas@vanderbilt.edu>
2017-06-06 09:30:43 -0500

    issue with savinf Form if empty checkbox

--- saveForm.php
+++ saveForm.php
@@ -16,13 +16,13 @@ $email_timestamp =  empty(ExternalModules::getProjectSetting($prefix, $pid, 'ema
 $email_condition =  empty(ExternalModules::getProjectSetting($prefix, $pid, 'email-condition'))?array():ExternalModules::getProjectSetting($prefix, $pid, 'email-condition');
 
 #checkboxes
-if(!isset($_REQUEST['email-repetitive'])){
+if(empty($_REQUEST['email-repetitive'])){
     $repetitive = 0;
 }else{
     $repetitive = 1;
 }
 
-if(!isset($_REQUEST['email-timestamp'])){
+if(empty($_REQUEST['email-timestamp'])){
     $timestamp = 0;
 }else{
     $timestamp = 1;
