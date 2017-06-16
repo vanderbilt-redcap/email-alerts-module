3926bcdb4216e9a158de53ec7ddaab8b36d5e2c8
bascome <eva.bascompte.moragas@vanderbilt.edu>
2017-06-16 11:15:16 -0500

    Modal dialog bigger on email preview

--- configure.php
+++ configure.php
@@ -626,7 +626,7 @@ $indexSubSet = sizeof($config['email-dashboard-settings'][0]['value']);
     </div>
 
     <div class="modal fade" id="external-modules-configure-modal-preview" tabindex="-1" role="dialog" aria-labelledby="Codes">
-        <div class="modal-dialog" role="document">
+        <div class="modal-dialog" role="document" style="width: 800px">
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close closeCustomModal" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
