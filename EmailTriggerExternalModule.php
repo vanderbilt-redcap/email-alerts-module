18ad381cfac7746eba7b29c9264af7f3619a386c
bascome <eva.bascompte.moragas@vanderbilt.edu>
2017-08-08 11:21:55 -0500

    table width 100%

--- EmailTriggerExternalModule.php
+++ EmailTriggerExternalModule.php
@@ -4,7 +4,6 @@ namespace Vanderbilt\EmailTriggerExternalModule;
 use ExternalModules\AbstractExternalModule;
 use ExternalModules\ExternalModules;
 
-//require_once dirname(__FILE__) . '/../../external_modules/classes/ExternalModules.php';
 require_once APP_PATH_DOCROOT.'Classes/Files.php';
 require_once 'vendor/autoload.php';
 require_once 'EmailTriggerExternalModule.php';
