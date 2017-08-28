f94532a17b25760ababbb47fc06a961f2518a153
bascome <eva.bascompte.moragas@vanderbilt.edu>
2017-08-28 10:52:11 -0500

    +Configuration message, survey must have return and saved enabled modified

--- check_survey_save_return_AJAX.php
+++ check_survey_save_return_AJAX.php
@@ -17,15 +17,20 @@ if(!empty($surveyLink_var)){
         $var = preg_split("/[;,]+/", $surveylink)[0];
         $button = preg_split("/[;,]+/", $surveylink)[1];
 
-        $instrument_form = str_replace('[', '', $var);
+        $instrument_form = str_replace('[SURVEYLINK_', '', $var);
         $instrument_form = str_replace(']', '', $instrument_form);
 
-        $sql = "SELECT survey_id from `redcap_surveys` where project_id = ".$project_id." AND form_name ='".$instrument_form."' AND save_and_return=0";
+        $sql = "SELECT save_and_return from `redcap_surveys` where project_id = ".$project_id." AND form_name ='".$instrument_form."'";
         $result = db_query($sql);
 
-        while($row = db_fetch_assoc($result)) {
-            $link = '<a href="'.APP_PATH_WEBROOT_FULL.APP_PATH_WEBROOT.'Surveys/edit_info.php?pid='.$project_id.'&view=showform&page=request&redirectDesigner=1" target="_blank"><u>enable "<strong>Save and Return</strong>" in Survey Settings</u></a>';
-            $messsage .= 'The survey titled "<strong>prescreening_survey</strong>" is not activated as a Save and Return survey. Please '.$link.' to use the link. If you want the survey to be editable, also select "Allow respondents to modify completed responses."<br/>';
+        if(!empty($row = db_fetch_assoc($result))) {
+            if($row['save_and_return'] == 0){
+                $link = '<a href="' . APP_PATH_WEBROOT_FULL . APP_PATH_WEBROOT . 'Surveys/edit_info.php?pid=' . $project_id . '&view=showform&page=request&redirectDesigner=1" target="_blank"><u>enable "<strong>Save and Return</strong>" in Survey Settings</u></a>';
+                $messsage .= 'The survey titled "<strong>'.$instrument_form.'</strong>" is not activated as a Save and Return survey. Please ' . $link . ' to use the link. If you want the survey to be editable, also select "Allow respondents to modify completed responses."<br/>';
+            }
+        }else{
+            $link = '<a href="' . APP_PATH_WEBROOT_FULL . APP_PATH_WEBROOT . 'Surveys/edit_info.php?pid=' . $project_id . '&view=showform&page=request&redirectDesigner=1" target="_blank"><u>enable "<strong>Save and Return</strong>" in Survey Settings</u></a>';
+            $messsage .= 'The survey titled "<strong>'.$instrument_form.'</strong>" is not activated as a Save and Return survey. Please ' . $link . ' to use the link. If you want the survey to be editable, also select "Allow respondents to modify completed responses."<br/>';
         }
     }
 
