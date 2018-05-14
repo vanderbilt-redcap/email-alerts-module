ca0d18c5760f1f6b6b315d769abcc2994450f93c
bascome <eva.bascompte.moragas@vumc.org>
2018-05-14 12:36:08 -0500

    Added directory in preview record to get the vendor path

--- previewRecordForm.php
+++ previewRecordForm.php
@@ -4,7 +4,7 @@ namespace Vanderbilt\EmailTriggerExternalModule;
 use ExternalModules\AbstractExternalModule;
 use ExternalModules\ExternalModules;
 
-require_once 'vendor/autoload.php';
+require_once __DIR__.'/vendor/autoload.php';
 
 $project_id = $_GET['pid'];
 $index =  $_REQUEST['index_modal_record_preview'];
