2e265eabe6d29b96d4f8ed5ea10114cf46f2baf1
bascome <eva.bascompte.moragas@vumc.org>
2018-05-15 14:05:34 -0500

    reenable queued elements

--- reEnableForm.php
+++ reEnableForm.php
@@ -17,6 +17,24 @@ if($active == "false"){
     $letter = "N";
 }
 
+#RE-enable queued alerts
+if(!empty($email_queue)){
+    $scheduled_records_changed = "";
+    $queue = $email_queue;
+    foreach ($email_queue as $id=>$email){
+        if($email['project_id'] == $pid && $email['alert']==$index){
+            $queue[$id]['deactivated'] = 0;
+            $scheduled_records_changed .= $email['record'].",";
+        }
+    }
+    $module->setProjectSetting('email-queue', $queue);
+
+    #Add logs
+    $action_description = "Deactivated Scheduled Alert ".$index;
+    $changes_made = "Record IDs re-enabled: ".rtrim($scheduled_records_changed,",");
+    \REDCap::logEvent($action_description,$changes_made,NULL,NULL,NULL,$pid);
+}
+
 
 $module->setProjectSetting('email-deleted', $email_deleted);
 
