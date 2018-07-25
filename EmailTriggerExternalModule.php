7c547b41f2ea677e0d6ef189a0c2e8ed066d77f6
bascome <eva.bascompte.moragas@vumc.org>
2018-07-25 12:02:11 -0500

    Cron testing scheduled with expiration. Added logs. Fixed bug on hasQueueExpired

--- EmailTriggerExternalModule.php
+++ EmailTriggerExternalModule.php
@@ -280,8 +280,6 @@ class EmailTriggerExternalModule extends AbstractExternalModule
                 $email_sent_total = 0;
                 foreach ($email_queue as $index=>$queue){
                     error_log("scheduledemails PID: ".$project_id." - hasQueueExpired queue:".$index." Alert#".$queue['alert']."? ");
-                    $expired = $this->hasQueueExpired($queue,$index);
-                    error_log("scheduledemails PID: ".$project_id." - hasQueueExpired: ".$expired." .... ".date("Y-m-d H:i:s"));
                     if($email_sent_total < 100 && !$this->hasQueueExpired($queue,$index)) {
                         error_log("scheduledemails PID: ".$project_id." - has not expired today ".date("Y-m-d H:i:s"));
                         if($queue['deactivated'] != 1 && $this->sendToday($queue, $index)){
@@ -456,33 +454,33 @@ class EmailTriggerExternalModule extends AbstractExternalModule
      * @param $project_id
      * @return bool
      */
-    function hasQueueExpired($queue,$index,$project_id){
-        error_log("scheduledemails PID: ".$project_id." - Inside hasQueueExpired");
-        /*$cron_queue_expiration_date =  empty($this->getProjectSetting('cron-queue-expiration-date',$queue['project_id']))?array():$this->getProjectSetting('cron-queue-expiration-date',$queue['project_id'])[$queue['alert']];
+    function hasQueueExpired($queue,$index){
+        error_log("scheduledemails PID: ".$queue['project_id']." - Inside hasQueueExpired");
+        $cron_queue_expiration_date =  empty($this->getProjectSetting('cron-queue-expiration-date',$queue['project_id']))?array():$this->getProjectSetting('cron-queue-expiration-date',$queue['project_id'])[$queue['alert']];
         $cron_queue_expiration_date_field =  empty($this->getProjectSetting('cron-queue-expiration-date-field',$queue['project_id']))?array():$this->getProjectSetting('cron-queue-expiration-date-field',$queue['project_id'])[$queue['alert']];
 
-        error_log("scheduledemails PID: ".$project_id." - Before eval logic");
+        error_log("scheduledemails PID: ".$queue['project_id']." - Before eval logic");
         $evaluateLogic = \REDCap::evaluateLogic($cron_queue_expiration_date_field, $queue['project_id'], $queue['record'], $queue['event_id']);
         if($queue['isRepeatInstrument']){
             $evaluateLogic = \REDCap::evaluateLogic($cron_queue_expiration_date_field,  $queue['project_id'], $queue['record'], $queue['event_id'], $queue['instance'], $queue['instrument']);
         }
 
-        error_log("scheduledemails PID: ".$project_id." - after eval logic");
+        error_log("scheduledemails PID: ".$queue['project_id']." - after eval logic");
         if($cron_queue_expiration_date == 'date' && $cron_queue_expiration_date_field != ""){
             if(strtotime($cron_queue_expiration_date_field) <= strtotime(date('Y-m-d'))){
-                $this->deleteQueuedEmail($index,$project_id);
-                error_log("scheduledemails PID: ".$project_id." - Delete date Expired queue ".$index." Alert#".$queue['alert']);
+                $this->deleteQueuedEmail($index,$queue['project_id']);
+                error_log("scheduledemails PID: ".$queue['project_id']." - Delete date Expired queue ".$index." Alert#".$queue['alert']);
                 return true;
             }
         }else if($cron_queue_expiration_date == 'cond' && $cron_queue_expiration_date_field != ""){
             if($evaluateLogic){
-                $this->deleteQueuedEmail($index,$project_id);
-                error_log("scheduledemails PID: ".$project_id." - Delete cond Expired queue ".$index." Alert#".$queue['alert']);
+                $this->deleteQueuedEmail($index,$queue['project_id']);
+                error_log("scheduledemails PID: ".$queue['project_id']." - Delete cond Expired queue ".$index." Alert#".$queue['alert']);
                 return true;
             }
         }
 
-        error_log("scheduledemails PID: ".$project_id." - Delete Expired FALSE queue ".$index." Alert#".$queue['alert']);*/
+        error_log("scheduledemails PID: ".$queue['project_id']." - Delete Expired FALSE queue ".$index." Alert#".$queue['alert']);
         return false;
     }
 
