bd262efd9bd4fc04db7ca424520367594f2ed5ee
bascome <eva.bascompte.moragas@vumc.org>
2019-12-02 15:34:57 -0600

    testing

--- EmailTriggerExternalModule.php
+++ EmailTriggerExternalModule.php
@@ -342,6 +342,7 @@ class EmailTriggerExternalModule extends AbstractExternalModule
         while($row = db_fetch_assoc($q)){
             $project_id = $row['project_id'];
             if($project_id != "") {
+                error_log("Test emailalerts ".$project_id);
                 $email_queue = $this->getProjectSetting('email-queue', $project_id);
                 $queue_aux = $email_queue;
                 if ($email_queue != '') {
