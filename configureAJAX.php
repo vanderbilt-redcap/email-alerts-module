db8c3fc0cb6fbf62c4b9f1af342c41cdd5c19f07
Kyle McGuffin <kyle.mcguffin@vumc.org>
2020-05-12 09:58:25 -0400

    Escape HTML special characters on saving configuration

--- configureAJAX.php
+++ configureAJAX.php
@@ -6,14 +6,14 @@ use ExternalModules\ExternalModules;
 
 require_once 'EmailTriggerExternalModule.php';
 
-$module->setProjectSetting('datapipe_label',$_POST['datapipe_label']);
-$module->setProjectSetting('datapipe_var',$_POST['datapipe_var']);
-$module->setProjectSetting('emailFromForm_var',$_POST['emailFromForm_var']);
-$module->setProjectSetting('datapipeEmail_var',$_POST['datapipeEmail_var']);
-$module->setProjectSetting('surveyLink_var',$_POST['surveyLink_var']);
-$module->setProjectSetting('formLink_var',$_POST['formLink_var']);
-$module->setProjectSetting('emailFailed_var',$_POST['emailFailed_var']);
-$module->setProjectSetting('emailSender_var',$_POST['emailSender_var']);
+$module->setProjectSetting('datapipe_label',htmlspecialchars($_POST['datapipe_label']));
+$module->setProjectSetting('datapipe_var',htmlspecialchars($_POST['datapipe_var']));
+$module->setProjectSetting('emailFromForm_var',htmlspecialchars($_POST['emailFromForm_var']));
+$module->setProjectSetting('datapipeEmail_var',htmlspecialchars($_POST['datapipeEmail_var']));
+$module->setProjectSetting('surveyLink_var',htmlspecialchars($_POST['surveyLink_var']));
+$module->setProjectSetting('formLink_var',htmlspecialchars($_POST['formLink_var']));
+$module->setProjectSetting('emailFailed_var',htmlspecialchars($_POST['emailFailed_var']));
+$module->setProjectSetting('emailSender_var',htmlspecialchars($_POST['emailSender_var']));
 
 
 echo json_encode(array(
