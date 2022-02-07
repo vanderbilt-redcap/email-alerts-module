a860f64ea07f081dedbbb8cec0cbbd2e38b3715a
knil-maloon <linkedfrends@gmail.com>
2022-02-07 13:36:54 -0600

    testing

--- configure.php
+++ configure.php
@@ -1348,7 +1348,7 @@ foreach ($language_errors as $err){
                         $info_modal[$index][$configRow['key']] = htmlspecialchars_decode($configRow['value'][$index],ENT_NOQUOTES);
                         if( $configRow['key'] == 'email-condition'){
                             error_log("EmailTesting value: ".$configRow['value'][$index]);
-                            error_log("EmailTesting value2: ". htmlspecialchars_decode($configRow['value'][$index],ENT_NOQUOTES));
+                            error_log("EmailTesting value2: ". html_entity_decode($configRow['value'][$index]));
                         }
                     }else{
                         $info_modal[$index][$configRow['key']] = $configRow['value'][$index];
