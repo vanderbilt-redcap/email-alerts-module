9f877aa3dbe3e5467937aae4a8f60e279a399532
knil-maloon <linkedfrends@gmail.com>
2023-12-08 08:54:35 -0600

    tinymce toolbar fix

--- js/globals.js
+++ js/globals.js
@@ -890,8 +890,8 @@ EmailAlerts.Settings.prototype.initializeRichTextFields = function(){
         menubar: false,
         branding: false,
         elementpath: false, // Hide this, since it oddly renders below the textarea.
-        plugins: ['autolink lists link image charmap hr anchor pagebreak searchreplace code fullscreen insertdatetime media nonbreaking table directionality imagetools'],
-        toolbar1: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify',
+        plugins: ['autolink', 'lists', 'link', 'image', 'charmap', 'hr', 'anchor', 'pagebreak', 'searchreplace', 'code', 'fullscreen', 'insertdatetime', 'media', 'nonbreaking', 'table', 'directionality', 'imagetools'],
+        toolbar1: 'undo redo | image | styles | bold italic | alignleft aligncenter alignright alignjustify',
         toolbar2: 'outdent indent | bullist numlist | table | forecolor backcolor | searchreplace fullscreen code',
         relative_urls : true, // force image urls to be absolute
         document_base_url : "http://www.example.com/path1/",
@@ -932,8 +932,8 @@ EmailAlerts.Settings.prototype.initializeRichTextFields = function(){
         menubar: false,
         branding: false,
         elementpath: false, // Hide this, since it oddly renders below the textarea.
-        plugins: ['autolink lists link image charmap hr anchor pagebreak searchreplace code fullscreen insertdatetime media nonbreaking table directionality imagetools'],
-        toolbar1: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify',
+        plugins: ['autolink', 'lists', 'link', 'image', 'charmap', 'hr', 'anchor', 'pagebreak', 'searchreplace', 'code', 'fullscreen', 'insertdatetime', 'media', 'nonbreaking', 'table', 'directionality', 'imagetools'],
+        toolbar1: 'undo redo | image | styles | bold italic | alignleft aligncenter alignright alignjustify',
         toolbar2: 'outdent indent | bullist numlist | table | forecolor backcolor | searchreplace fullscreen code',
         relative_urls : true, // force image urls to be absolute
         document_base_url : "http://www.example.com/path1/",
